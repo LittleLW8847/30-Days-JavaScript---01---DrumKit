ce958a26a97c5ea14df36211259ea5c4b6216268
LittleLW8847 <31029060+LittleLW8847@users.noreply.github.com>
2024-01-29 00:44:30 +0700

    javascript hurts my head

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-window.addEventListener('keydown', function(e){
-    console.log(e);
-});
+function playSound(e){
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    
+    if(!audio){
+        return; //stop fucntion from running
+    }
+    audio.currentTime = 0; //allow you to spam same sound by reset the time of the track to 0
+    audio.play();
+}
+
+window.addEventListener('keydown', playSound);
