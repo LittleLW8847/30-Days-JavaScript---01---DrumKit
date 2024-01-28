f90a03048b35ec97a61c7ecf1a27d9dcbfb632f7
LittleLW8847 <31029060+LittleLW8847@users.noreply.github.com>
2024-01-29 00:48:42 +0700

    ahhhhhhh leave me aloneeee

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 function playSound(e){
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    console.log(key);
     if(!audio){
         return; //stop fucntion from running
     }
     audio.currentTime = 0; //allow you to spam same sound by reset the time of the track to 0
     audio.play();
+    key.classList.add('playing');
 }
 
 window.addEventListener('keydown', playSound);
